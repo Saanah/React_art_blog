b85910328000fe0b9458a47e220c9d02b383ceed
Saana Pullola <t0pusa00@students.oamk.fi>
2023-04-29 19:30:33 +0300

    Routing for Github pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,14 @@ import React from 'react';
 import styles from '../styles/Header.module.css'
 import Dropdown from './Dropdown';
 
-function Header(props) {
+function Header() {
     return (
         <div className={styles.header}>
             <ul>
-                <li><a href="/">HOME</a></li>
-                <li><a href="artist">ARTIST</a></li>
-                <li><a href="tools">SOFTWARE AND BRUSHES</a></li>
-                <li><a href="contact">CONTACT ME!</a></li>
+                <li><a href="./">HOME</a></li>
+                <li><a href="#/artist">ARTIST</a></li>
+                <li><a href="#/tools">SOFTWARE AND BRUSHES</a></li>
+                <li><a href="#/contact">CONTACT ME!</a></li>
                 <li><Dropdown/></li>
             </ul>
         </div>
